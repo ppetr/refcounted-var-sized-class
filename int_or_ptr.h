27c637f97c1913a34f6d62abb45962986736c201
Petr <petr.mvd+git@gmail.com>
2022-08-14 21:53:52 +0200

    Extend `IntOrPtr` to distinguish between `T` (move-only) and `const T` (copyable) type parameters

--- int_or_ptr.h
+++ int_or_ptr.h
@@ -26,67 +26,129 @@
 #include "ref.h"
 
 namespace refptr {
+namespace internal {
 
-// Provides a variant type between an `intptr_t` (without its most significant
-// bit) and a reference-counted, const pointer. Relies on the fact that an
-// allocated pointer is aligned and therefore always has zero least significant
-// bit.
-//
-// TODO: Extend this so that `const T` is copyable and plain `T` is move-only.
-template <typename T>
-class IntOrPtr {
+template <typename Ptr>
+class IntOrValue {
  public:
   static_assert(__has_feature(cxx_unrestricted_unions),
-                "IntOrPtr relies on the unrestricted unions feature");
-  static_assert(
-      sizeof(intptr_t) == sizeof(Ref<const T>),
-      "Internal error: Ref<const T> must have the same size as intptr_t");
-  IntOrPtr() : IntOrPtr(0) {}
-  explicit IntOrPtr(intptr_t value) : number_((value << 1) | 1) {
-    assert(has_number());
-  }
-  explicit IntOrPtr(Ref<const T> ref) : ref_(Ref<const T>(std::move(ref))) {
-    assert(has_ref());
+                "IntOrValue relies on the unrestricted unions feature");
+  static_assert(sizeof(intptr_t) == sizeof(Ptr),
+                "Internal error: Ptr must have the same size as intptr_t");
+  IntOrValue() : IntOrValue(0) {}
+  explicit IntOrValue(intptr_t value) : number_((value << 1) | 1) {
+    assert(ref() == nullptr);
   }
   template <typename... Args>
-  explicit IntOrPtr(absl::in_place_t, Args&&... args)
-      : IntOrPtr(New<T, Args...>(std::forward<Args>(args)...).Share()) {}
+  explicit IntOrValue(absl::in_place_t, Args&&... args)
+      : value_(std::forward<Args>(args)...) {
+    assert(ref() != nullptr);
+  }
 
-  IntOrPtr(IntOrPtr&& that) : IntOrPtr() { *this = std::move(that); }
-  IntOrPtr(const IntOrPtr& that) : IntOrPtr() { *this = that; }
-  IntOrPtr& operator=(IntOrPtr&& that) {
+  IntOrValue(IntOrValue&& that) : IntOrValue() { *this = std::move(that); }
+  IntOrValue(const IntOrValue& that) : IntOrValue() { *this = that; }
+  IntOrValue& operator=(IntOrValue&& that) {
     std::swap(number_, that.number_);
     return *this;
   }
-  IntOrPtr& operator=(const IntOrPtr& that) {
+  IntOrValue& operator=(const IntOrValue& that) {
     Clear();
     if (that.has_ref()) {
-      new (&ref_) Ref<const T>(that.ref_);
+      new (&value_) Ptr(that.value_);
     } else {
       number_ = that.number_;
     }
     return *this;
   }
 
-  ~IntOrPtr() { Clear(); }
+  ~IntOrValue() { Clear(); }
 
-  bool has_number() const { return ref() == nullptr; }
-  bool has_ref() const { return ref() != nullptr; }
+  bool has_number() const { return number_ & 1; }
+  bool has_ref() const { return !has_number(); }
 
   absl::optional<intptr_t> number() const {
     return has_number() ? absl::make_optional(number_ >> 1) : absl::nullopt;
   }
-  const T* ref() const {
+
+  const Ptr* ref() const {
     // Here we rely on `cxx_unrestricted_unions`: We query `number_` in all
-    // cases, even if the populated value is `ref_`.
-    return (number_ & 1) ? nullptr : &*ref_;
+    // cases, even if the populated value is `value_`.
+    return has_ref() ? &value_ : nullptr;
+  }
+  Ptr* ref() { return has_ref() ? &value_ : nullptr; }
+
+  // TODO: Add `operator==` and `!=`.
+
+ private:
+  void Clear() {
+    if (ref() != nullptr) {
+      value_.~Ref();
+      number_ = 0;
+    }
   }
 
-  absl::variant<intptr_t, std::reference_wrapper<const T>> Variant() const {
-    using result = absl::variant<intptr_t, std::reference_wrapper<const T>>;
-    const T* ptr = ref();
+  union {
+    intptr_t number_;
+    Ptr value_;
+  };
+};
+
+}  // namespace internal
+
+// Provides a variant type between an `intptr_t` (without its most significant
+// bit) and a reference-counted, const pointer. Relies on the fact that an
+// allocated pointer is aligned and therefore always has zero least significant
+// bit.
+//
+// If `T` is a `const` type then instances are cheaply copyable and movable.
+// Otherwise `T` is (cheaply) movable-only.
+//
+// Therefore, instances should be always passed by value.
+//
+// The class has a very small memory footprint - the same as a plain pointer.
+template <typename T>
+class IntOrPtr {
+ public:
+  IntOrPtr() : IntOrPtr(0) {}
+  explicit IntOrPtr(intptr_t value) : value_(value) { assert(has_number()); }
+  explicit IntOrPtr(Ref<T> ref) : value_(absl::in_place, std::move(ref)) {
+    assert(has_ref());
+  }
+  template <typename... Args>
+  explicit IntOrPtr(absl::in_place_t, Args&&... args)
+      : IntOrPtr(New<absl::remove_const_t<T>, Args...>(
+            std::forward<Args>(args)...)) {}
+  template <typename U = std::remove_const<T>,
+            typename std::enable_if<!std::is_same<U, T>::value, int>::type = 0>
+  IntOrPtr(IntOrPtr<U>&& unique) {
+    auto* ref_ptr = unique.value_.ref();
+    if (ref_ptr == nullptr) {
+      value_ = internal::IntOrValue<Ref<T>>(*unique.number());
+    } else {
+      value_ =
+          internal::IntOrValue<Ref<T>>(absl::in_place, std::move(*ref_ptr));
+    }
+  }
+
+  IntOrPtr(IntOrPtr&& that) = default;
+  IntOrPtr(const IntOrPtr& that) = default;
+  IntOrPtr& operator=(IntOrPtr&& that) = default;
+  IntOrPtr& operator=(const IntOrPtr& that) = default;
+
+  bool has_number() const { return value_.has_number(); }
+  bool has_ref() const { return value_.has_ref(); }
+  absl::optional<intptr_t> number() const { return value_.number(); }
+
+  T* ref() {
+    Ref<T>* ref_ptr = value_.ref();
+    return (ref_ptr == nullptr) ? nullptr : &**ref_ptr;
+  }
+
+  absl::variant<intptr_t, std::reference_wrapper<T>> Variant() {
+    using result = absl::variant<intptr_t, std::reference_wrapper<T>>;
+    T* ptr = ref();
     if (ptr == nullptr) {
-      return result(absl::in_place_index<0>, number_ >> 1);
+      return result(absl::in_place_index<0>, *number());
     } else {
       return result(absl::in_place_index<1>, *ptr);
     }
@@ -94,19 +156,12 @@ class IntOrPtr {
 
   // TODO: Add `operator==` and `!=`.
 
- private:
-  void Clear() {
-    if (has_ref()) {
-      ref_.~Ref();
-      number_ = 0;
-    }
-  }
+  internal::IntOrValue<Ref<T>> value_;
 
-  union {
-    intptr_t number_;
-    Ref<const T> ref_;
-  };
+  friend class IntOrPtr<typename std::remove_const<T>::type>;
 };
+
+// TODO: Add a similar class that wraps a type-safe enum `EnumOrPtr<E, T>`.
 
 }  // namespace refptr
 
--- int_or_ptr_test.cc
+++ int_or_ptr_test.cc
@@ -41,7 +41,7 @@ struct Foo {
 };
 
 TEST(IntOrPtrTest, ConstructionByDefault) {
-  IntOrPtr<Foo> value;
+  IntOrPtr<const Foo> value;
   ASSERT_TRUE(value.has_number());
   ASSERT_FALSE(value.has_ref());
   EXPECT_THAT(value.number(), Optional(0));
@@ -49,7 +49,7 @@ TEST(IntOrPtrTest, ConstructionByDefault) {
 }
 
 TEST(IntOrPtrTest, ConstructionOfNumber) {
-  IntOrPtr<Foo> value(42);
+  IntOrPtr<const Foo> value(42);
   ASSERT_TRUE(value.has_number());
   ASSERT_FALSE(value.has_ref());
   EXPECT_THAT(value.number(), Optional(42));
@@ -58,17 +58,17 @@ TEST(IntOrPtrTest, ConstructionOfNumber) {
 }
 
 TEST(IntOrPtrTest, MoveAndCopyConstructionOfNumber) {
-  IntOrPtr<Foo> value(42);
-  IntOrPtr<Foo> copied(value);
+  IntOrPtr<const Foo> value(42);
+  IntOrPtr<const Foo> copied(value);
   EXPECT_THAT(copied.Variant(), VariantWith<intptr_t>(42));
-  IntOrPtr<Foo> moved(std::move(value));
+  IntOrPtr<const Foo> moved(std::move(value));
   EXPECT_THAT(moved.Variant(), VariantWith<intptr_t>(42));
 }
 
 TEST(IntOrPtrTest, ConstructionOfObject) {
   int counter = 0;
   {
-    IntOrPtr<Foo> value(absl::in_place, counter, 42);
+    IntOrPtr<const Foo> value(absl::in_place, counter, 42);
     EXPECT_EQ(counter, 1);
     ASSERT_FALSE(value.has_number());
     ASSERT_TRUE(value.has_ref());
@@ -80,19 +80,38 @@ TEST(IntOrPtrTest, ConstructionOfObject) {
   EXPECT_EQ(counter, 0);
 }
 
-TEST(IntOrPtrTest, MoveAndCopyConstructionOfObject) {
+TEST(IntOrPtrTest, CopyConstructionOfObject) {
+  int counter = 0;
+  {
+    IntOrPtr<const Foo> value(absl::in_place, counter, 42);
+    EXPECT_EQ(counter, 1);
+    IntOrPtr<const Foo> copy(value);
+    EXPECT_THAT(copy.Variant(), VariantWith<std::reference_wrapper<const Foo>>(
+                                    Field(&Foo::value_, 42)));
+    EXPECT_EQ(counter, 1);
+  }
+  EXPECT_EQ(counter, 0);
+}
+
+TEST(IntOrPtrTest, MoveConstructionOfObject) {
   int counter = 0;
   {
     IntOrPtr<Foo> value(absl::in_place, counter, 42);
     EXPECT_EQ(counter, 1);
-    IntOrPtr<Foo> copied(value);
-    EXPECT_THAT(copied.Variant(),
-                VariantWith<std::reference_wrapper<const Foo>>(
-                    Field(&Foo::value_, 42)));
-    EXPECT_EQ(counter, 1);
     IntOrPtr<Foo> moved(std::move(value));
-    EXPECT_THAT(moved.Variant(), VariantWith<std::reference_wrapper<const Foo>>(
+    EXPECT_THAT(moved.Variant(), VariantWith<std::reference_wrapper<Foo>>(
                                      Field(&Foo::value_, 42)));
+    EXPECT_EQ(counter, 1);
+  }
+  EXPECT_EQ(counter, 0);
+}
+
+TEST(IntOrPtrTest, MoveConstructionOfConstFromNonConst) {
+  EXPECT_THAT(IntOrPtr<const Foo>(IntOrPtr<Foo>(42)).number(), Optional(42));
+  int counter = 0;
+  {
+    IntOrPtr<const Foo> object(IntOrPtr<Foo>(absl::in_place, counter, 73));
+    EXPECT_THAT(object.ref(), Pointee(Field(&Foo::value_, 73)));
     EXPECT_EQ(counter, 1);
   }
   EXPECT_EQ(counter, 0);
@@ -102,7 +121,7 @@ TEST(IntOrPtrTest, MoveAssignment) {
   IntOrPtr<Foo> value;
   int counter = 0;
   value = IntOrPtr<Foo>(absl::in_place, counter, 42);
-  EXPECT_THAT(value.Variant(), VariantWith<std::reference_wrapper<const Foo>>(
+  EXPECT_THAT(value.Variant(), VariantWith<std::reference_wrapper<Foo>>(
                                    Field(&Foo::value_, 42)));
   EXPECT_EQ(counter, 1);
   value = IntOrPtr<Foo>(73);
@@ -110,11 +129,23 @@ TEST(IntOrPtrTest, MoveAssignment) {
   EXPECT_EQ(counter, 0);
 }
 
+TEST(IntOrPtrTest, MoveAssignmentOfConst) {
+  IntOrPtr<const Foo> value;
+  int counter = 0;
+  value = IntOrPtr<const Foo>(absl::in_place, counter, 42);
+  EXPECT_THAT(value.Variant(), VariantWith<std::reference_wrapper<const Foo>>(
+                                   Field(&Foo::value_, 42)));
+  EXPECT_EQ(counter, 1);
+  value = IntOrPtr<const Foo>(73);
+  EXPECT_THAT(value.Variant(), VariantWith<intptr_t>(73));
+  EXPECT_EQ(counter, 0);
+}
+
 TEST(IntOrPtrTest, CopyAssignment) {
-  IntOrPtr<Foo> value;
+  IntOrPtr<const Foo> value;
   int counter = 0;
   {
-    IntOrPtr<Foo> copy(absl::in_place, counter, 42);
+    IntOrPtr<const Foo> copy(absl::in_place, counter, 42);
     value = copy;
     EXPECT_EQ(counter, 1);
   }
@@ -122,7 +153,7 @@ TEST(IntOrPtrTest, CopyAssignment) {
   EXPECT_THAT(value.Variant(), VariantWith<std::reference_wrapper<const Foo>>(
                                    Field(&Foo::value_, 42)));
   {
-    IntOrPtr<Foo> copy(73);
+    IntOrPtr<const Foo> copy(73);
     value = copy;
     EXPECT_EQ(counter, 0);
   }
