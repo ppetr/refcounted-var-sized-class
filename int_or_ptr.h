4e41517421ca24c841ab781b053d2e56ae4252b7
Petr <petr.mvd+git@gmail.com>
2023-10-07 22:40:31 +0200

    Add a type parameter to `IntOrPtr` to allow changing the underlying integer type
    Reformat using `clang-format`.

--- int_or_ptr.h
+++ int_or_ptr.h
@@ -38,18 +38,18 @@ class IntOrValue {
     assert(ptr() == nullptr);
   }
   template <typename... Args>
-  explicit IntOrValue(absl::in_place_t, Args&&... args)
+  explicit IntOrValue(absl::in_place_t, Args &&...args)
       : value_(std::forward<Args>(args)...) {
     assert(ptr() != nullptr);
   }
 
-  IntOrValue(IntOrValue&& that) : IntOrValue() { *this = std::move(that); }
-  IntOrValue(const IntOrValue& that) : IntOrValue() { *this = that; }
-  IntOrValue& operator=(IntOrValue&& that) {
+  IntOrValue(IntOrValue &&that) : IntOrValue() { *this = std::move(that); }
+  IntOrValue(const IntOrValue &that) : IntOrValue() { *this = that; }
+  IntOrValue &operator=(IntOrValue &&that) {
     std::swap(number_, that.number_);
     return *this;
   }
-  IntOrValue& operator=(const IntOrValue& that) {
+  IntOrValue &operator=(const IntOrValue &that) {
     Clear();
     if (that.has_ptr()) {
       new (&value_) Ptr(that.value_);
@@ -68,12 +68,12 @@ class IntOrValue {
     return has_number() ? absl::make_optional(number_ >> 1) : absl::nullopt;
   }
 
-  const Ptr* ptr() const {
+  const Ptr *ptr() const {
     // Here we rely on `cxx_unrestricted_unions`: We query `number_` in all
     // cases, even if the populated value is `value_`.
     return has_ptr() ? &value_ : nullptr;
   }
-  Ptr* ptr() { return has_ptr() ? &value_ : nullptr; }
+  Ptr *ptr() { return has_ptr() ? &value_ : nullptr; }
 
  private:
   void Clear() {
@@ -102,22 +102,31 @@ class IntOrValue {
 // Therefore, instances should be always passed by value.
 //
 // The class has a very small memory footprint - the same as a plain pointer.
-template <typename T>
+template <typename T, typename I = intptr_t>
 class IntOrPtr {
  public:
+  static_assert(sizeof(I) >= sizeof(void *),
+                "A pointer won't fit into the type parameter I");
+#if defined(__STDCPP_DEFAULT_NEW_ALIGNMENT__)
+  // See https://stackoverflow.com/a/57296359/1333025.
+  static_assert(__STDCPP_DEFAULT_NEW_ALIGNMENT__ > 1,
+                "The default new alighment is just 1");
+
+#endif
+
   IntOrPtr() : IntOrPtr(0) {}
-  explicit IntOrPtr(intptr_t value) : value_(value) { assert(has_number()); }
+  explicit IntOrPtr(I value) : value_(value) { assert(has_number()); }
   explicit IntOrPtr(Ref<T> ptr) : value_(absl::in_place, std::move(ptr)) {
     assert(has_ptr());
   }
   template <typename... Args>
-  explicit IntOrPtr(absl::in_place_t, Args&&... args)
+  explicit IntOrPtr(absl::in_place_t, Args &&...args)
       : IntOrPtr(New<absl::remove_const_t<T>, Args...>(
             std::forward<Args>(args)...)) {}
   template <typename U = std::remove_const<T>,
             typename std::enable_if<!std::is_same<U, T>::value, int>::type = 0>
-  IntOrPtr(IntOrPtr<U>&& unique) {
-    auto* value = unique.value_.ptr();
+  IntOrPtr(IntOrPtr<U> &&unique) {
+    auto *value = unique.value_.ptr();
     if (value == nullptr) {
       value_ = internal::IntOrValue<Ref<T>>(*unique.number());
     } else {
@@ -125,23 +134,23 @@ class IntOrPtr {
     }
   }
 
-  IntOrPtr(IntOrPtr&& that) = default;
-  IntOrPtr(const IntOrPtr& that) = default;
-  IntOrPtr& operator=(IntOrPtr&& that) = default;
-  IntOrPtr& operator=(const IntOrPtr& that) = default;
+  IntOrPtr(IntOrPtr &&that) = default;
+  IntOrPtr(const IntOrPtr &that) = default;
+  IntOrPtr &operator=(IntOrPtr &&that) = default;
+  IntOrPtr &operator=(const IntOrPtr &that) = default;
 
   bool has_number() const { return value_.has_number(); }
   bool has_ptr() const { return value_.has_ptr(); }
-  absl::optional<intptr_t> number() const { return value_.number(); }
+  absl::optional<I> number() const { return value_.number(); }
 
-  T* ptr() const {
-    Ref<T>* value = const_cast<Ref<T>*>(value_.ptr());
+  T *ptr() const {
+    Ref<T> *value = const_cast<Ref<T> *>(value_.ptr());
     return (value == nullptr) ? nullptr : &**value;
   }
 
-  absl::variant<intptr_t, std::reference_wrapper<T>> Variant() const {
-    using result = absl::variant<intptr_t, std::reference_wrapper<T>>;
-    T* value = ptr();
+  absl::variant<I, std::reference_wrapper<T>> Variant() const {
+    using result = absl::variant<I, std::reference_wrapper<T>>;
+    T *value = ptr();
     if (value == nullptr) {
       return result(absl::in_place_index<0>, *number());
     } else {
@@ -149,19 +158,19 @@ class IntOrPtr {
     }
   }
 
-  bool operator==(const IntOrPtr& other) const {
-    T* value = ptr();
+  bool operator==(const IntOrPtr &other) const {
+    T *value = ptr();
     if (value == nullptr) {
       return number() == other.number();
     } else {
-      T* other_value = other.ptr();
+      T *other_value = other.ptr();
       if (other_value == nullptr) {
         return false;
       }
       return *value == *other_value;
     }
   }
-  bool operator!=(const IntOrPtr& other) const { return !(*this == other); }
+  bool operator!=(const IntOrPtr &other) const { return !(*this == other); }
 
   internal::IntOrValue<Ref<T>> value_;
 
